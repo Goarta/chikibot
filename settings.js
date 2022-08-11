4030e5f60af9ed0254ac182490b5e4e9a2da4c5d
Goarta <111064151+Goarta@users.noreply.github.com>
2022-08-11 20:28:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
+//© 2022 Chiki bot Inc. Chiki Bot MD
 //Thank you to Lord Buddha, Family and Myself
 //════════════════════════════//
 //recode kar ke youtube pe upload kar rhe hai ya
@@ -36,10 +36,10 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6282162837259'] //ur owner number
+global.ownername = "Zuki" //ur owner name
+global.ytname = "YT: Chiki" //ur yt chanel name
+global.socialm = "GitHub: DGChiki" //ur github or insta name
+global.location = "Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['6282162837259']
+global.ownertag = '6282162837259' //ur tag number
+global.botname = 'Chiki Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Chiki Bot MD4 Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "Chiki bot MD4" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,25 +79,25 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
-global.ntilinkytvid = []
-global.ntilinkytch = []
-global.ntilinkig = []
-global.ntilinkfb = []
-global.ntilinktg = []
-global.ntilinktt = []
-global.ntilinktwt = []
-global.ntilinkall = []
-global.nticall = []
-global.ntwame = []
-global.nttoxic = []
-global.ntnsfw = []
-global.ntvirtex = []
+global.premium = ['6282162837259'] //ur premium numbers
+global.antilinkytvid = []
+global.antilinkytch = []
+global.antilinkig = []
+global.antilinkfb = []
+global.antilinktg = []
+global.antilinktt = []
+global.antilinktwt = []
+global.antilinkall = []
+global.anticall = []
+global.antiwame = []
+global.antitoxic = []
+global.antnsfw = []
+global.antvirtex = []
 global.rkyt = []
-global.wlcm = []
+global.welcome = []
 global.gcrevoke = []
 global.autorep = []
-global.ntilink = []
+global.antilinkgc = []
 
 //omther
 global.sessionName = 'session'
@@ -105,33 +105,33 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'Sucses ✓',
+    admin: 'Fitur Ini Hanya Untuk Admin!',
+    botAdmin: Bot Harus Admin Dulu!',
+    owner: 'Fitur Ini Hanya Untuk Owner!',
+    group: 'Fitur Hanya Digunakan Untuk Grup!',
+    private: 'Fitur Hanya Digunakan Untuk Obrolan Pribadi!',
+    bot: 'Fitur Ini Hanya Untuk Bot',
+    wait: 'Sedang Prosess  ...',
+    linkm: 'Linknya Mana boss?',
+    error: 'Kesalahan!!',
+    endLimit: 'No Limit',
+    ban: 'Anda telah diblokir oleh owner, jika Anda ingin diblokir, chat owner.',
+    nsfw: 'Fitur nsfw belum diaktifkan, silahkan hubungi admin untuk mengaktifkan ',
+    banchat: 'Bot diblokir di grup ini, harap hubungi owner untuk membatalkan pemblokiran'
 }
     global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
+    premium: "No Limit",
+    free: {
+    monayawal: {
 }
    global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+   darahawal: {,
+   besiawal: {,
+   goldawal: {,
+   emeraldawal: {,
+   umpanawal: {,
+   potionawal: {
 }
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
